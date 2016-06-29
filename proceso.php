e62f03f79710be7fb03151fc0c8a78c3f0c60a39
MaximianoMendezMorales <maxi.enero.1997@gmail.com>
2016-06-28 20:27:13 -0600

    proyecto Web funcionando hasta validacion de Usuario

---
+++ proceso.php
@@ -0,0 +1,17 @@
+<?php
+include_once "control/usuarios.php";
+$clsUsuarios = new usuarios();
+
+if(isset($_POST['txtusr']) && isset($_POST['txtpwd'])){
+	$currUserVO= $clsUsuarios->getByUserPass($_POST['txtusr'],$_POST['txtpwd']);
+	if(sizeof($currUserVO) > 0) {
+		foreach ($currUserVO as $UserVO) {
+    		echo "Bienvenido ". $UserVO->getUsername();
+		}
+    }
+	else{
+			echo "El usuario ".$_POST['txtusr']. " es incorrecto";
+	}
+}
+
+
